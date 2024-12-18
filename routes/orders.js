d9a743be6809dfabb91ee5977d2279b544fab31e
IanCaballes <16100337@usc.edu.ph>
2024-12-18 22:53:16 +0800

    Fixed reference error in order routes

--- routes/orders.js
+++ routes/orders.js
@@ -4,9 +4,9 @@ const ordersController = require('../controllers/orderscontroller');
 const { authMiddleware, verifyRBAC, routeLog} = require('../middleware/authRBAC.js');
 
 router.post('/', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), ordersController.createOrder);
-router.get('/', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), orderController.getAllOrders);
-router.get('/:id', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), orderController.getOrderById);
-router.put('/:id', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), orderController.updateOrder);
-router.delete('/:id', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), orderController.deleteOrder);
+router.get('/', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), ordersController.getAllOrders);
+router.get('/:id', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), ordersController.getOrderById);
+router.put('/:id', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), ordersController.updateOrder);
+router.delete('/:id', authMiddleware, verifyRBAC(['Admin', 'Sales']), routeLog('Order post route accessed'), ordersController.deleteOrder);
 
 module.exports = router;
